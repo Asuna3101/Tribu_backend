5e12062ae08e1ec933ec145598f8bab8d421878b
Asuna3101 <20210274@aloe.ulima.edu.pe>
2024-12-05 16:37:31 -0500

    postbyId

--- routes/post.rb
+++ routes/post.rb
@@ -1,13 +1,13 @@
 get '/posts/lista' do
   begin
-    # Consulta para obtener los datos con JOIN entre las tablas
+    # Query to get all posts with JOINs for additional information
     posts = DB[:posts]
             .join(:usuarios, id: :usuario_id)
             .join(:carreras, id: Sequel[:usuarios][:carrera_id])
             .join(:materiales, post_id: Sequel[:posts][:id])
             .select(
               Sequel[:usuarios][:nombre].as(:nombre_usuario),
-              Sequel[:usuarios][:foto].as(:foto_usuario), # Ajustado el alias aquí
+              Sequel[:usuarios][:foto].as(:foto_usuario),
               Sequel[:carreras][:nombre].as(:carrera),
               Sequel[:posts][:descripcion].as(:descripcion_post),
               Sequel[:materiales][:enlace].as(:enlace_material),
@@ -17,17 +17,54 @@ get '/posts/lista' do
             .all
 
     if posts.any?
-      # Devolver los datos en formato JSON
+      # Return data in JSON format
       status 200
       posts.to_json
     else
-      # Si no hay publicaciones
+      # Return 404 if no posts exist
       status 404
-      { message: 'No hay publicaciones registradas.' }.to_json
+      { message: 'No posts registered.' }.to_json
     end
   rescue StandardError => e
-    # Manejo de errores
+    # Handle errors
     status 500
-    { error: "Error en el servidor: #{e.message}" }.to_json
+    { error: "Server error: #{e.message}" }.to_json
+  end
+end
+
+get '/posts/usuario/:id' do
+  usuario_id = params[:id].to_i # Get the user ID from the parameters
+
+  begin
+    # Query to get posts associated with the specific user
+    posts = DB[:posts]
+            .join(:usuarios, id: :usuario_id)
+            .join(:carreras, id: Sequel[:usuarios][:carrera_id])
+            .join(:materiales, post_id: Sequel[:posts][:id])
+            .where(Sequel[:posts][:usuario_id] => usuario_id) # Filter by usuario_id
+            .select(
+              Sequel[:usuarios][:nombre].as(:nombre_usuario),
+              Sequel[:usuarios][:foto].as(:foto_usuario),
+              Sequel[:carreras][:nombre].as(:carrera),
+              Sequel[:posts][:descripcion].as(:descripcion_post),
+              Sequel[:materiales][:enlace].as(:enlace_material),
+              Sequel[:posts][:fecha_subida_post].as(:fecha_subida)
+            )
+            .order(Sequel.desc(:fecha_subida_post))
+            .all
+
+    if posts.any?
+      # Return data in JSON format
+      status 200
+      posts.to_json
+    else
+      # Return 404 if no posts exist for the user
+      status 404
+      { message: "No posts registered for the user with ID #{usuario_id}." }.to_json
+    end
+  rescue StandardError => e
+    # Handle errors
+    status 500
+    { error: "Server error: #{e.message}" }.to_json
   end
 end
