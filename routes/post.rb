1b4feb830f2019e8a7f1eb26435a719523f0eac0
Asuna3101 <20210274@aloe.ulima.edu.pe>
2024-12-05 01:37:30 -0500

    db

--- routes/post.rb
+++ routes/post.rb
@@ -1,22 +1,32 @@
-# Endpoint para obtener todos los posts, ordenados del más reciente al más antiguo
 get '/posts/lista' do
-    begin
-      # Recuperar todos los posts y ordenarlos por fecha de subida (más reciente primero)
-      posts = Post.order(Sequel.desc(:fecha_subida_post)).all
-  
-      if posts.any?
-        # Convertir a JSON y devolver el resultado
-        status 200
-        posts.to_json
-      else
-        # Si no hay posts, retornar un mensaje indicando la ausencia
-        status 404
-        { message: 'No hay publicaciones registradas.' }.to_json
-      end
-    rescue StandardError => e
-      # Manejo de errores
-      status 500
-      { error: "Error en el servidor: #{e.message}" }.to_json
+  begin
+    # Consulta para obtener los datos con JOIN entre las tablas
+    posts = DB[:posts]
+            .join(:usuarios, id: :usuario_id)
+            .join(:carreras, id: Sequel[:usuarios][:carrera_id])
+            .join(:materiales, post_id: Sequel[:posts][:id])
+            .select(
+              Sequel[:usuarios][:nombre].as(:nombre_usuario),
+              Sequel[:carreras][:nombre].as(:carrera),
+              Sequel[:posts][:descripcion].as(:descripcion_post),
+              Sequel[:materiales][:enlace].as(:enlace_material),
+              Sequel[:posts][:fecha_subida_post].as(:fecha_subida)
+            )
+            .order(Sequel.desc(:fecha_subida_post))
+            .all
+
+    if posts.any?
+      # Devolver los datos en formato JSON
+      status 200
+      posts.to_json
+    else
+      # Si no hay publicaciones
+      status 404
+      { message: 'No hay publicaciones registradas.' }.to_json
     end
+  rescue StandardError => e
+    # Manejo de errores
+    status 500
+    { error: "Error en el servidor: #{e.message}" }.to_json
   end
-  ##############################################################################################
+end
