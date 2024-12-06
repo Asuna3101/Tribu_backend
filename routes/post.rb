7306a2b8c6f7c111c1467db2c86d8880dedf5bbc
Asuna3101 <20210274@aloe.ulima.edu.pe>
2024-12-05 21:11:20 -0500

    new endpoint

--- routes/post.rb
+++ routes/post.rb
@@ -34,4 +34,40 @@ get '/posts/lista' do
   end
 end
 
+get '/posts/usuario/:id' do
+  usuario_id = params[:id].to_i # Get the user ID from the parameters
 
+  begin
+    # Query to get posts associated with the specific user
+    posts = DB[:posts]
+            .join(:usuarios, id: :usuario_id)
+            .join(:carreras, id: Sequel[:usuarios][:carrera_id])
+            .join(:materiales, post_id: Sequel[:posts][:id])
+            .where(Sequel[:posts][:usuario_id] => usuario_id) # Filter by usuario_id
+            .select(
+              Sequel[:usuarios][:nombre].as(:nombre_usuario),
+              Sequel[:usuarios][:foto].as(:foto_usuario),
+              Sequel[:carreras][:nombre].as(:carrera),
+              Sequel[:posts][:descripcion].as(:descripcion_post),
+              Sequel[:materiales][:enlace].as(:enlace_material),
+              Sequel[:posts][:fecha_subida_post].as(:fecha_subida),
+              Sequel[:posts][:id].as(:post_id),
+            )
+            .order(Sequel.desc(:fecha_subida_post))
+            .all
+
+    if posts.any?
+      # Return data in JSON format
+      status 200
+      posts.to_json
+    else
+      # Return 404 if no posts exist for the user
+      status 404
+      { message: "No posts registered for the user with ID #{usuario_id}." }.to_json
+    end
+  rescue StandardError => e
+    # Handle errors
+    status 500
+    { error: "Server error: #{e.message}" }.to_json
+  end
+end
